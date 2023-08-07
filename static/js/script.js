69bf82b936c9d6307ce2ea0f6c648277d8ec6fac
Pratik Aryal <83208684+aryalpatrick@users.noreply.github.com>
2023-08-07 15:06:02 +0545

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,14 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const fileInput = document.getElementById('images');
+    const fileLabel = document.getElementById('file-label');
+  
+    fileInput.addEventListener('change', (event) => {
+      const filesSelected = event.target.files;
+      if (filesSelected.length > 0) {
+        fileLabel.textContent = `${filesSelected.length} file(s) selected`;
+      } else {
+        fileLabel.textContent = 'Click to choose Images';
+      }
+    });
+  });
+  
